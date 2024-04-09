ece4ac0377ef479875eac6dd13b1cc47a307086b
csorbazoli <csorbazoli@gmail.com>
2024-04-09 09:21:26 +0100

    add junit 4 and easy mock dependencies to demo project

--- junit-tools-demo/src/main/java/com/example/junittoolsdemo/models/DemoObject.java
+++ junit-tools-demo/src/main/java/com/example/junittoolsdemo/models/DemoObject.java
@@ -5,17 +5,24 @@ public class DemoObject {
 	private String demoString;
 	private int demoInt;
 
+	public DemoObject getThis() {
+		return this;
+	}
+
 	public String getDemoString() {
 		return demoString;
 	}
+
 	public void setDemoString(String demoString) {
 		this.demoString = demoString;
 	}
+
 	public int getDemoInt() {
 		return demoInt;
 	}
+
 	public void setDemoInt(int demoInt) {
 		this.demoInt = demoInt;
 	}
-	
+
 }
